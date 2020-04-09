276000d3ff36986516abd23851901851e349c64e
Ryan Thompson <ryan@pyrocms.com>
2020-04-08 23:03:47 -0500

    Vuetify

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -2,11 +2,33 @@
     assets('scripts.js')->add('public::vendor/anomaly/field_type/textarea/js/index.js');
 @endphp
 
-<textarea-field-type {!! html_attributes($fieldType->attributes([
-    'autogrow' => $fieldType->config('autogrow'),
-    'maxlength' => $fieldType->config('max'),
-    'rows' => $fieldType->config('rows', 3),
-])) !!}/>
+<v-input>
+    <v-textarea
+    {!! html_attributes([
+        'value'    => $fieldType->getValue(),
+        'label'    => $fieldType->getLabel(),
+        'name'     => $fieldType->getInputName(),
+        'messages' => $fieldType->getInstructions(),
+        
+        'autogrow'  => (bool)$fieldType->config('autogrow'),
+        'rows'      => $fieldType->config('rows', 3),
+        'maxlength' => $fieldType->config('max'),
+
+        'readonly' => $fieldType->isReadonly() ? true : false,
+        'disabled' => $fieldType->isDisabled() ? true : false,
+    
+        // 'data-field'      => $fieldType->getField(),
+        // 'data-provides'   => $fieldType->getNamespace(),
+        // 'data-field_name' => $fieldType->getFieldName(),
+    
+        // 'class' => $fieldType->getClass(),
+        // 'id'    => $fieldType->getInputName(),
+    ]) !!}
+    
+    outlined
+    clearable
+    ></v-textarea>
+</v-input>
 
 {{-- <textarea {!! html_attributes($fieldType->attributes()) !!}>{{ $fieldType->value }}</textarea> --}}
 
